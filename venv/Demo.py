180b2e284c4937b3ba6264de38d1c24c922669a5
Slawomir <gliba@wp.pl>
2019-07-11 10:01:41 +0200

    ff

--- venv/Demo.py
+++ venv/Demo.py
@@ -6,6 +6,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(r"C:\Users\slawek\Downloads\selenium_pliki\Test.html")
 driver.maximize_window()
-driver.find_element_by_xpath('//*[@id="newPage"]').click()
+#driver.find_element_by_xpath('//*[@id="newPage"]').click()
 #driver.quit()   # zamyka wszystkie okna, metoda .close() zamyka okno na ktorej byl focus
-driver.close()
+#driver.close()
+driver.find_element(By)
+driver.find_element_by_id("fname").click()
